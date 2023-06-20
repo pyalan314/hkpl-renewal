2cc43acdc18def421db87e88e9b5ba51c193e1b6
pyalan314 <pyalan314@gmail.com>
2023-06-21 02:17:33 +0800

    refactor common functions

---
+++ common.py
@@ -0,0 +1,64 @@
+from collections import namedtuple
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+
+Record = namedtuple('Record', ['name', 'due_date', 'times', 'value'])
+Page = namedtuple('Page', ['form_id', 'action', 'records', 'valid_records'])
+
+
+def prepare_login_data(username, password):
+    data = {
+        'USER': username,
+        'PASSWORD': password,
+        'target': '/auth/login?target=/',
+        'lang': 'tc',
+        'chamo': '',
+        'entryPage': '/tc/login.php',
+        'queryString': '',
+        'pwExpiryAction': '',
+        'noPwExpiryAlert': ''
+    }
+    return '&'.join(f'{k}={v}' for k, v in data.items())
+
+
+def prepare_renew_data(form_id, book_values):
+    data = {
+        f'{form_id}_hf_0': '',
+        'renewalCheckboxGroup:checkoutsTable:topToolbars:toolbars:1:span:pageSize:sizeChoice': 0,
+        'renewalCheckboxGroup:checkoutsTable:bottomToolbars:toolbars:2:span:pageSize:sizeChoice': 0,
+    }
+    part_1 = '&'.join(f'{k}={v}' for k, v in data.items())
+    part_2 = '&'.join(f'renewalCheckboxGroup={value}' for value in book_values)
+    return '&'.join((part_1, part_2))
+
+
+def parse_check_out(html):
+    soup = BeautifulSoup(html, 'lxml')
+    table = soup.find('table', {'id': 'checkout'})
+    form = table.find_parent('form')
+    t_body = table.find('tbody')
+    rows = t_body.find_all('tr')
+
+    def parse_row(row):
+        cols = row.find_all('td')
+        renew_input = cols[0].find('input')
+        if renew_input:
+            value = renew_input['value']
+        else:
+            value = None
+        return Record(
+            name=cols[1].text.strip()[:20],
+            due_date=datetime.strptime(cols[4].text.strip(), '%Y-%m-%d').date(),
+            value=value,
+            times=cols[5].text.strip().split(' ')[0]
+        )
+
+    records = [parse_row(x) for x in rows if len(x.find_all('td')) > 1]
+    valid_records = [x for x in records if x.value]
+    return Page(
+        form_id=form['id'],
+        action=form['action'],
+        records=records,
+        valid_records=valid_records,
+    )
--- main.py
+++ main.py
@@ -1,129 +1,83 @@
 import os
-from collections import namedtuple
-from datetime import datetime
+import time
 
 import requests
 import urllib3
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from icecream import ic
+from loguru import logger
+
+import common
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 load_dotenv()
-
-Record = namedtuple('Record', ['name', 'due_date', 'times', 'value'])
-Page = namedtuple('Page', ['form_id', 'action', 'records'])
 
 
 class Client:
 
     def __init__(self):
-        self.session = requests.Session()
-        self.session.verify = False
+        self.session = self.init_session()
+
+    @staticmethod
+    def init_session():
+        s = requests.Session()
+        # HKPL cert cannot be verified
+        s.verify = False
+        return s
 
     def login(self, username, password):
+        # Use a new session for a new login
+        self.session = self.init_session()
         url = 'https://www.hkpl.gov.hk/tc/login.html'
         r = self.session.get(url)
+        assert r.status_code == 200
 
         url = 'https://www.hkpl.gov.hk/iw/login.php'
-        data = dict(
-            USER=username,
-            PASSWORD=password,
-            traget='/auth/login?target=/',
-            lang='tc',
-            chamo='',
-            entryPage='/tc/login.php',
-            queryString='',
-            pwExpiryAction='',
-            noPwExpiryAlert='',
-        )
-        data = f'USER={username}&PASSWORD={password}&target=%2Fauth%2Flogin%3Ftarget%3D%2F&lang=tc&chamo=&entryPage=%2Ftc%2Flogin.php&queryString=&pwExpiryAction=&noPwExpiryAlert='
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
+        data = common.prepare_login_data(username, password)
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         r = self.session.post(url, data=data, allow_redirects=False, headers=headers)
         # ic(dict(r.request.headers))
         # ic(r.request.body)
         # ic(r.url)
         # ic(r.status_code)
         # ic(dict(r.headers))
-        ic(dict(self.session.cookies))
-
-        # self.session.get('https://www.hkpl.gov.hk/auth/login?target=/', verify=False, allow_redirects=False)
-        #
-        # self.session.get('https://www.hkpl.gov.hk/', verify=False)
+        logger.debug(dict(self.session.cookies))
+        assert r.status_code == 302
+        assert 'SMSESSION' in r.cookies
 
     def read_checkout(self):
-        # self.session.get('https://www.hkpl.gov.hk/tc/index.html', verify=False)
         r = self.session.get('https://webcat.hkpl.gov.hk/wicket/bookmarkable/com.vtls.chamo.webapp.component.patron.PatronAccountPage?theme=WEB&locale=zh_TW')
         return r.text
 
-    def renew(self, form_id, action, book_value):
+    def renew(self, form_id, action, book_values):
         url = action.replace('../', 'https://webcat.hkpl.gov.hk/wicket/')
-        data = {
-            f'{form_id}_hf_0': '',
-            'renewalCheckboxGroup:checkoutsTable:topToolbars:toolbars:1:span:pageSize:sizeChoice': 0,
-            'renewalCheckboxGroup:checkoutsTable:bottomToolbars:toolbars:2:span:pageSize:sizeChoice': 0,
-            'renewalCheckboxGroup': book_value,
-        }
-        data = f'{form_id}_hf_0=&renewalCheckboxGroup%3AcheckoutsTable%3AtopToolbars%3Atoolbars%3A1%3Aspan%3ApageSize%3AsizeChoice=0&renewalCheckboxGroup%3AcheckoutsTable%3AbottomToolbars%3Atoolbars%3A2%3Aspan%3ApageSize%3AsizeChoice=0&renewalCheckboxGroup={book_value}'
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
-        ic(dict(self.session.cookies))
+        data = common.prepare_renew_data(form_id, book_values)
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        # ic(dict(self.session.cookies))
         r = self.session.post(url, data=data, allow_redirects=False, headers=headers)
-        ic(dict(r.request.headers))
-        ic(r.request.body)
-        ic(r.status_code)
-        ic(r.url)
-        ic(dict(r.headers))
-
-
-def analyze(html):
-    soup = BeautifulSoup(html, 'lxml')
-    table = soup.find('table', {'id': 'checkout'})
-    form = table.find_parent('form')
-    t_body = table.find('tbody')
-    rows = t_body.find_all('tr')
-
-    def parse_row(row):
-        cols = row.find_all('td')
-        ic(cols)
-        renew_input = cols[0].find('input')
-        if renew_input:
-            value = renew_input['value']
-        else:
-            value = None
-        return Record(
-            name=cols[1].text.strip(),
-            due_date=datetime.strptime(cols[4].text.strip(), '%Y-%m-%d').date(),
-            value=value,
-            times=cols[5].text.strip().split(' ')[0]
-        )
-
-    records = [parse_row(x) for x in rows if len(x.find_all('td')) > 1]
-    return Page(
-        form_id=form['id'],
-        action=form['action'],
-        records=records,
-    )
+        logger.debug(r.status_code)
+        logger.debug(r.headers)
+        assert r.status_code == 302
 
 
 def main():
     client = Client()
     username = os.environ['HKPL_USERNAME']
     password = os.environ['HKPL_PASSWORD']
-    client.login(username, password)
-    text = client.read_checkout()
-    with open('temp.html', 'w', encoding='utf-8') as f:
-        f.write(text)
-    with open('temp.html', 'r', encoding='utf-8') as f:
-        text = f.read()
-    page = analyze(text)
-    ic(page)
-    valid_records = [x for x in page.records if x.value]
-    if valid_records:
-        client.renew(page.form_id, page.action, valid_records[0].value)
+    while True:
+        client.login(username, password)
+        text = client.read_checkout()
+        # with open('temp.html', 'w', encoding='utf-8') as f:
+        #     f.write(text)
+        # with open('temp.html', 'r', encoding='utf-8') as f:
+        #     text = f.read()
+        page = common.parse_check_out(text)
+        logger.info(page)
+        if page.valid_records:
+            logger.info(f'Renew {"|".join(x.name for x in page.valid_records)}')
+            # client.renew(page.form_id, page.action, [x.value for x in page.valid_records])
+        else:
+            logger.info('No valid record')
+        time.sleep(60)
 
 
 if __name__ == '__main__':
