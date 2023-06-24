ed5c4feb9e04b0a2d6732cc342bd7997f408b1ed
pyalan314 <pyalan314@gmail.com>
2023-06-24 11:35:08 +0800

    Make telegram config optional

--- notification.py
+++ notification.py
@@ -1,26 +1,35 @@
 import os
 
 from dotenv import load_dotenv
+from loguru import logger
 from notifiers import get_notifier
 
 from common import Record
 
 load_dotenv()
 
-CHAT_ID = os.environ['HKPL_CHAT_ID']
-BOT_TOKEN = os.environ['HKPL_BOT_TOKEN']
-service = get_notifier('telegram')
+CHAT_ID = os.environ.get('HKPL_CHAT_ID')
+BOT_TOKEN = os.environ.get('HKPL_BOT_TOKEN')
+
+if CHAT_ID and BOT_TOKEN:
+    service = get_notifier('telegram')
+else:
+    logger.debug('Notification is not available')
+    service = None
 
 
 def notify(msg, formatted=False):
     if formatted:
         msg = f"`{msg}`"
-    response = service.notify(
-        message=msg,
-        chat_id=CHAT_ID,
-        token=BOT_TOKEN,
-        parse_mode='markdown'
-    )
+    if service:
+        response = service.notify(
+            message=msg,
+            chat_id=CHAT_ID,
+            token=BOT_TOKEN,
+            parse_mode='markdown'
+        )
+        if not response.ok:
+            logger.error(response.errors)
 
 
 def send_no_action(records: list[Record]):
